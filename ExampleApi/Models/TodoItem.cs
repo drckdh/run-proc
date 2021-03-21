09a57141dc91d4fb5fd66a14af5ef519fa28d6df
drckdh <kevin.hogan@deadriver.com>
2021-03-21 16:23:28 -0400

    renaming

--- ExampleApi/Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-namespace ExampleApi.Models
-{
-    public class ExampleItem
-    {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
