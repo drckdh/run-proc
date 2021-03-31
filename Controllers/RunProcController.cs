1bde03168f5f63cc42cf23b24d806ed7707c0e81
drckdh <kevin.hogan@deadriver.com>
2021-03-31 19:32:13 -0400

    removed unused crud

--- Controllers/RunProcController.cs
+++ Controllers/RunProcController.cs
@@ -24,48 +24,6 @@ namespace RunProcApi.Controllers
             _dapper = dapper;  
         }  
 
-        [HttpPost(nameof(Create))]  
-        public async Task<int> Create(Parameters data)  
-        {  
-            var dbparams = new DynamicParameters();  
-            dbparams.Add("Id", data.Id, DbType.Int32);  
-            var result = await Task.FromResult(_dapper.Insert<int>("[dbo].[SP_Add_Artic le]"  
-                , dbparams,  
-                commandType: CommandType.StoredProcedure));  
-            return result;  
-        }  
-        [HttpGet(nameof(GetById))]  
-        public async Task<Parameters> GetById(int Id)  
-        {  
-            var result = await Task.FromResult(_dapper.Get<Parameters>($"Select * from [widgets] where Id = {Id}", null, commandType: CommandType.Text));  
-            return result;  
-        }  
-        [HttpDelete(nameof(Delete))]  
-        public async Task<int> Delete(int Id)  
-        {  
-            var result = await Task.FromResult(_dapper.Execute($"Delete [widgets] Where Id = {Id}", null, commandType: CommandType.Text));  
-            return result;  
-        }  
-        [HttpGet(nameof(Count))]  
-        public Task<int> Count(int num)  
-        {  
-            var totalcount = Task.FromResult(_dapper.Get<int>($"select COUNT(*) from [widgets] WHERE Age like '%{num}%'", null,  
-                    commandType: CommandType.Text));  
-            return totalcount;  
-        }  
-        [HttpPatch(nameof(Update))]  
-        public Task<int> Update(Parameters data)  
-        {  
-            var dbPara = new DynamicParameters();  
-            dbPara.Add("Id", data.Id);  
-            dbPara.Add("Name", data.Name, DbType.String);  
-  
-            var updateArticle = Task.FromResult(_dapper.Update<int>("[dbo].[SP_Update_Article]",  
-                            dbPara,  
-                            commandType: CommandType.StoredProcedure));  
-            return updateArticle;  
-        }  
-
         [HttpPost("Query")]
         public List<dynamic> Query( JObject jsonInput, string procName)  
         {  
